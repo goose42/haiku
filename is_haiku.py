6e3897860d7fdbec44279a61f9f1cdd6512b03b2
vjayanth <vjayanth@indiana.edu>
2013-07-25 00:04:51 -0400

    something, every little of it though, works

--- is_haiku.py
+++ is_haiku.py
@@ -8,9 +8,32 @@ dict = cmudict.dict()
 def sy_count(word):
   word = word.lower()
   #there must be a better way to check if a char is a number
-  syl_list = [list (punct for punct in pron if str(punct[-1]).isdigit()) 
-    for pron in dict[word]]
-  #need to get a better way to assess the syllable count, as in "best fit" for a sentence
-  return len(syl_list[0]) 
+  sy_list_list = []
+  for pron in dict[word]:
+    last_char = ''
+    sy_list = []
+    for punct in pron:
+      if str(punct[-1]).isdigit() or (len(last_char)==1 and len(punct)==1 and len(sy_list)!=0):
+        sy_list.append(punct)
+      last_char = punct
+    sy_list_list.append(sy_list)
+  #syl_list = [list (punct for punct in pron if str(punct[-1]).isdigit()) 
+  #  for pron in dict[word]]
+  
+  
+  #need to get a better way to assess the syllable count, 
+  #as in "best fit" for a sentence
+  return max([len(x) for x in sy_list_list])  
 
-
+def is_haiku(sentence):
+  #best to add something here to check if the sentence is english
+  words = nltk.word_tokenize(sentence)
+  syllables = 0
+  for word in words:
+    print word, sy_count(word)
+    syllables += sy_count(word)
+  print "Total syllables = " , syllables
+  if syllables == 17:
+    return True
+  else:
+    return False
